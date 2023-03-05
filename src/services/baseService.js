bce1f4a859fc28b5f97cab22c37be3e9b87ca808
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-03-05 23:30:21 +0700

    update base url

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://shoesshop.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
